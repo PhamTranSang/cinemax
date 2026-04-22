c01a5471583754ad99f12fe6d8b856a4028ec062
stran <bocay113@gmail.com>
2026-04-22 11:44:53 +0700

    feat(backend): initialize backend structure with Spring Boot and PostgreSQL configuration

---
+++ backend/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.5"
+	id("io.spring.dependency-management") version "1.1.7"
+	// id("org.graalvm.buildtools.native") version "0.10.5"
+}
+
+group = "app.cinemax"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+// graalvmNative {
+// 	toolchainDetection.set(true)
+// 	binaries {
+// 		named("main") {
+// 			imageName.set("cinemax")
+// 			mainClass.set("app.cinemax.CinemaxApplication")
+// 			javaLauncher.set(javaToolchains.launcherFor {
+// 				languageVersion.set(JavaLanguageVersion.of(21))
+// 				vendor.set(JvmVendorSpec.GRAAL_VM)
+// 			})
+// 			buildArgs.add("--verbose")
+// 		}
+// 	}
+// }
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-h2console")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-liquibase")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-liquibase-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testCompileOnly("org.projectlombok:lombok")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
